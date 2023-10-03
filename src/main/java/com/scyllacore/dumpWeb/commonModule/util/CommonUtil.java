e0d8b2e2138323fc558300b9c5c6bcc979b1d236
myungsikyoon <yms8795@gmail.com>
2023-10-03 10:31:16 +0900

    refactor : [BE] CommonUtil 코드 정리.

--- src/main/java/com/scyllacore/dumpWeb/commonModule/util/CommonUtil.java
+++ src/main/java/com/scyllacore/dumpWeb/commonModule/util/CommonUtil.java
@@ -1,16 +1,12 @@
 package com.scyllacore.dumpWeb.commonModule.util;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scyllacore.dumpWeb.commonModule.db.dto.login.Login;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -23,42 +19,8 @@ public class CommonUtil {
         return request.getSession(true);
     }
 
-    /**
-     * FUNCTION :: Ajax요청에 대한 리턴 Map 객체 선언
-     * @return
-     */
-    public HashMap<String, Object> returnMap() {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("httpCode", 500);
-        return map;
-    }
-
-    /**
-     * FUNCTION :: JSON 문자열로 변환
-     * @return
-     */
-    public String jsonFormatTransfer(Map<String, Object> map) {
-        String result = "";
-        ObjectMapper mapper = new ObjectMapper();
-
-        try {
-            result = mapper.writeValueAsString(map);
-        } catch (Exception e) {
-            log.error("Exception ["+ e.getMessage() + "]");
-        }
-        return result;
-    }
-
-    public Map<String, Object> mapFormatTransfer(String json) {
-        ObjectMapper mapper = new ObjectMapper();
-        HashMap<String, Object> map = new HashMap<>();
-        try {
-           map =  mapper.readValue(json, new TypeReference<HashMap<String, Object>>() {});
-
-        } catch (Exception e) {
-            log.error("Exception ["+ e.getMessage() +"]");
-        }
-        return map;
+    public Login getLoginInfoBySession() {
+        return (Login) request.getSession(true).getAttribute("loginInfo");
     }
 
 
