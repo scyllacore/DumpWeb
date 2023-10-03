71da22ffe4a3d59ab7e7309c58a64f3fed91c6c8
myungsikyoon <yms8795@gmail.com>
2023-10-03 23:17:56 +0900

    feat : [BE] loginService http응답 구현.

--- src/main/java/com/scyllacore/dumpWeb/loginModule/service/LoginService.java
+++ src/main/java/com/scyllacore/dumpWeb/loginModule/service/LoginService.java
@@ -2,6 +2,7 @@ package com.scyllacore.dumpWeb.loginModule.service;
 
 import com.scyllacore.dumpWeb.commonModule.db.dto.login.LoginDTO;
 import com.scyllacore.dumpWeb.commonModule.db.mapper.login.LoginMapper;
+import com.scyllacore.dumpWeb.commonModule.http.dto.ResponseDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -13,24 +14,24 @@ public class LoginService {
 
     private final LoginMapper loginMapper;
 
-    public String login(LoginDTO loginInfo, HttpServletRequest request) {
+    public ResponseDTO<String> login(LoginDTO loginInfo, HttpServletRequest request) {
 
         LoginDTO login = loginMapper.selectUserInfoForValidCheck(loginInfo);
 
         if (login == null) {
-            return "등록되지 않은 ID 입니다.";
+            return new ResponseDTO<String>(403, "등록되지 않은 ID 입니다.");
         }
 
         login = loginMapper.selectUserInfo(loginInfo);
 
         if (login == null) {
-            return "패스워드가 일치하지 않습니다.";
+            return new ResponseDTO<String>(403, "비밀번호가 일치하지 않습니다.");
         }
 
         HttpSession session = request.getSession();
         session.setAttribute("loginInfo", login);
 
-        return login.getUserType();
+        return new ResponseDTO<String>(200, login.getUserType());
     }
 
 
