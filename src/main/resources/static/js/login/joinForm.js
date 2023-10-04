00ae6ffc82bac093f9f3d8e4afcbd39904408cc9
myungsikyoon <yms8795@gmail.com>
2023-10-04 21:14:59 +0900

    feat : [FE] personalPopUp 구현.

---
+++ src/main/resources/static/js/login/joinForm.js
@@ -0,0 +1,13 @@
+function openPersonalInfoNoticePopUp(page) {
+    document.querySelector('.' + page).style.display = 'block';
+}
+
+function closePersonalInfoNoticePopUp(page) {
+    document.querySelector('.' + page).style.display = 'none';
+}
+
+function join(){
+
+
+
+}
