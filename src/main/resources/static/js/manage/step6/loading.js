7d58a527eed268c83e2e1e603c43eaf416909eb2
scyllacore <yms8795@gmail.com>
2023-10-13 14:28:28 +0900

    refactor : [FE] 최종 실행 func 재정의 완료.

--- src/main/resources/static/js/manage/step6/loading.js
+++ src/main/resources/static/js/manage/step6/loading.js
@@ -2,3 +2,15 @@ let manageHandler;
 document.addEventListener("DOMContentLoaded", () =>{
     manageHandler = new ManageHandler(paramContainer);
 })
+
+const func = {
+    async listRetrieval(){
+       await manageHandler.listRetrieval('step6');
+    },
+    async approveInBulk(){
+        await manageHandler.modifyPaymentChkInBulk('step6','approve');
+    },
+    async cancelInBulk(){
+        await manageHandler.modifyPaymentChkInBulk('step6','cancel');
+    }
+}
