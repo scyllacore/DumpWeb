027076940ff12648cd5ff79c56548e3fb3b946c0
scyllacore <yms8795@gmail.com>
2024-04-29 14:55:47 +0900

    fix : [FileDTO] 정상적인 Builder 사용을 위해 NoArgsConstructor access 타입을 PROTECTED로 변경.

--- src/main/java/com/scyllacore/dumpWeb/commonModule/db/dto/manage/FileDTO.java
+++ src/main/java/com/scyllacore/dumpWeb/commonModule/db/dto/manage/FileDTO.java
@@ -1,11 +1,12 @@
 package com.scyllacore.dumpWeb.commonModule.db.dto.manage;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FileDTO {
 
     private Long fileId;
